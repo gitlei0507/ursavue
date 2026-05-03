5b7abe6ce7f019945769b7caa8bd683cb11c1ff2
gitlei0507 <skywing.wlei@gmail.com>
2026-05-03 10:12:34 +0800

    test

---
+++ packages/ursacomponents/src/components/UrsaProcess/ProcessView.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-dialog v-model="dialogVisible" title="查看流程图" width="800px">
+        <h2>测试</h2>
+    </el-dialog>
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        modelValue: {
+            type: Boolean,
+            default: false
+        },
+    })
+
+    const dialogVisible = computed({
+        get: () => props.modelValue,
+        set: (value) => emit('update:modelValue', value)
+    })
+
+</script>
+
+<style scoped></style>
